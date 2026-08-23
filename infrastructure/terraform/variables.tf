6f9d8c6a1705e867edba787663bff6a9b56999fd
Yash-Rajoliya <90317866+Yash-Rajoliya@users.noreply.github.com>
2026-08-24 04:04:52 +0530

    fix(infra): correct Terraform variable defaults and environment configs

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,3 +1,17 @@
 variable "region" {
-  default = "ap-south-1"
+  type        = string
+  description = "The AWS deployment region for infrastructure resources"
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  default     = "dev"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Name of the project used for resource naming and tagging"
+  default     = "djqueue"
 }
